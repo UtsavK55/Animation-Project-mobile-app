9c527abf77154c49c48a0c3e4b090f5940ffcab1
UtsavK55 <utsav@gkmit.co>
2024-11-14 09:40:37 +0530

    project-setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
